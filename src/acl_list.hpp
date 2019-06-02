903d184be0970966fafbaa446665797825a7b9cd
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2019-06-02 15:53:45 +0200

    Translate macro

--- src/acl_list.hpp
+++ src/acl_list.hpp
@@ -16,8 +16,8 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,  USA
 */
-#ifndef LLISTA_ACL_HPP
-#define LLISTA_ACL_HPP
+#ifndef ACL_LIST_HPP
+#define ACL_LIST_HPP
 #include "acl_element_kind.hpp"
 #include "cellrenderer_acl.hpp"
 #include <config.hpp>
