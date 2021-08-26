96fbd782cdfa4c47d7f6b91789e92a62614f7e6b
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2021-08-26 16:39:00 +0200

    Make the help available even if installed in non-standard locations

--- src/eiciel_container.cpp
+++ src/eiciel_container.cpp
@@ -90,7 +90,7 @@ void EicielContainer::show_help()
 #ifdef USING_GNOME2
     gnome_help_display("eiciel", NULL, &error);
 #else
-    gtk_show_uri_on_window(this->gobj(), "ghelp:eiciel", GDK_CURRENT_TIME,
+    gtk_show_uri_on_window(this->gobj(), "ghelp:" EICIELDOCDIR, GDK_CURRENT_TIME,
         &error);
 #endif
     if (error != NULL) {
